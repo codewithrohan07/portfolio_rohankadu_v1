9cfc0d32370a2f1e532fcf099965f3de93fdbfd2
codewithrohan07 <rohankadu1999@gmail.com>
2022-09-22 20:23:06 +0530

    header hero and about section done

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Rohan's Portfolio</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
+
+      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+      {/* Experience */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
+    </div>
+  );
+};
+
+export default Home;
